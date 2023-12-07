c540c533e02ba742ed62a71cf3f03dfcc7a507ea
Gabrielgsw <ggermanow279@gmail.com>
2023-12-07 09:18:54 -0300

    gitignore added

--- Matrizes/q4.c
+++ Matrizes/q4.c
@@ -5,24 +5,32 @@
 int main(void){
     srand(time(NULL));
 
-    int matriz[5][5];
-    
-    int countRows = 0,countColumns = 0;
+    int tam = 5;
 
-    for(int i = 0;i<5;i++){
-        for(int j = 0;j<5;j++){
-            matriz[i][j] = rand()%26;
+    int matriz[tam][tam], int vect[tam*tam], int count = 0;
+    
+    
+
+    for(int i = 0;i<tam*tam;i++){
+        vect[i] = rand()%100;
+    }
+
+    for(int i = 0;i<tam*tam;i++){
+        if(vect[count] == vect[i]){
+            vect[i] = rand()%100;
         }
     }
 
+
+
     
 
     
 
 
 
-    for(int i = 0;i<5;i++){
-        for(int j = 0;j<5;j++){
+    for(int i = 0;i<tam;i++){
+        for(int j = 0;j<tam;j++){
             printf("%d ",matriz[i][i]);
         }
         printf("\n");
---
+++ Matrizes/q6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void){
+
+    int rows = 5, int columns = 4,int tamanho = 30;
+    int info[rows][columns];
+    char nomes[rows][tamanho]
+
+
+    for(int i = 0;i<rows;i++){
+        printf("Digite o nome do aluno %i: ",(i+1));
+        scanf("%s",nomes[i]);
+    }
+
+
+    for(int i = 0;i<rows;i++){
+        for(int j = 0;j<columns;j++){
+            switch (j){
+                case 0:
+                    printf("Digite o numero de matricula: ");
+                    scanf("%i",&)
+            }
+                
+        }
+    }
+}
