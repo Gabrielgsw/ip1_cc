4ee7be4e50534406203bda15a83020d2875f9c73
Gabrielgsw <ggermanow279@gmail.com>
2023-12-26 21:06:59 -0300

    terminando os metodos jogo da velha

--- Jogos/JogoDaVeia.c
+++ Jogos/JogoDaVeia.c
@@ -46,10 +46,10 @@ void incializarTabuleiro(){
 
 // Funcao para obter a jogada do player
 
-void jogadaPlayer(){
+void jogadaPlayer(char letra){
 
     int x,y;
-    char letra;
+    
 
     printf("Digite as coordenadas(x,y): ");
     scanf("%d%d",&x,&y);
@@ -60,7 +60,7 @@ void jogadaPlayer(){
 
     if(tabuleiro[x][y] != ' '){
         printf("Esta posicao eh invalida ou ja foi preenchida! \n");
-        jogadaPlayer();
+        jogadaPlayer(letra);
     }else{
         tabuleiro[x][y] = letra;
     }
@@ -68,10 +68,10 @@ void jogadaPlayer(){
 
 // Funcao para os movimentos do pc
 
-void jogadasComputador(){
+void jogadasComputador(char letraPC){
 
     int i,j;
-    char letraPC;
+    
 
     for(int i = 0;i<3;i++){
         for(int j = 0;j<3;j++){
@@ -88,4 +88,53 @@ void jogadasComputador(){
         tabuleiro[i][j] == letraPC;
     }
 
+}
+
+// Mostrando o tabuleiro no formato
+
+void mostrarTabuleiro(){
+
+    int t;
+
+    for(t = 0; t<3; t++){
+        printf(" %c | %c | %c ",tabuleiro[t][0], tabuleiro[t][1],tabuleiro[t][2]);
+        if(t != 2){
+            printf("\n---|---|---\n");
+        }
+    }
+    printf("\n");
+
+}
+
+// Verificando se há vencedor
+
+char checarVencedor(){
+
+    int i;
+
+    // Verificando as linhas
+    for(i=0;i<3;i++){
+        if(tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][0] == tabuleiro[i][2]){
+            return tabuleiro[i][0];
+        }
+    }
+
+    // Verificando as colunas
+
+    for(i=0;i<3;i++){
+        if(tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[0][i] == tabuleiro[2][i]){
+            return tabuleiro[0][1];
+        }
+    }
+
+    // Verificando as diagonais
+    if(tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[0][0] == tabuleiro[2][2]){
+        return tabuleiro[0][0];
+    }
+
+    if(tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[0][2] == tabuleiro[2][2]){
+        return tabuleiro[0][2];
+    }
+
+    return ' ';
 }
